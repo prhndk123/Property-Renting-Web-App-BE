221d18961f9766bbaa461f0a38373911b9f7aa89
zaenal-blip <arif4321456789@gmail.com>
2026-04-29 21:58:04 +0700

    fix: refactor cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,9 +3,6 @@ import { CorsOptions } from "cors";
 export const corsOptions: CorsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:8000",
-    "http://localhost:3000",
     "https://rentivo.tmmin.online",
     process.env.BASE_FRONTEND_URL!,
     process.env.FRONTEND_URL!,
