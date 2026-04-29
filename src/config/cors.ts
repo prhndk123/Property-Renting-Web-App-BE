174baca2aadabe6be8c31d1c61bf6c67113f4716
ZAENAL ARIFIN <arif4321456789@gmail.com>
2026-04-29 20:44:43 +0700

    Review (#16)
    * feat: add review and report, and fixing notification email
    
    * feat: add seed to add property
    
    * fix: fix deploy
    
    * fix: error cors policy

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,10 @@ export const corsOptions: CorsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:8000",
+    "http://localhost:3000",
+    "https://rentivo.tmmin.online",
     process.env.BASE_FRONTEND_URL!,
-  ],
+    process.env.FRONTEND_URL!,
+  ].filter(Boolean),
   credentials: true,
 };
